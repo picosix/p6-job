1434110a9c6560ebef508e23afc8c5a853fa12d0
picosix <picosix.com@gmail.com>
2018-01-19 21:44:59 +0700

    refactor test case

--- src/graphql/User/tests/find.test.js
+++ src/graphql/User/tests/find.test.js
@@ -3,50 +3,15 @@ const _ = require("lodash");
 const bluebird = require("bluebird");
 
 const server = require("./server");
+const createUser = require("./_createUser");
 
 describe("Find one user", () => {
   let _id;
 
-  beforeAll(() => {
-    const user = {
-      username: faker.internet.userName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-      status: 0,
-      profile: {
-        firstName: faker.name.findName(),
-        lastName: faker.name.lastName(),
-        avatar: faker.image.avatar()
-      }
-    };
-
-    return server(
-      JSON.stringify({
-        query: `
-         mutation addUser($username: String!, $email: String!, $password: String!, $status: Int!, $profile: UserProfile) {
-          user: addUser(username: $username, email: $email, password: $password, status: $status, profile: $profile) {
-            _id
-            username
-            email
-            password
-            status
-            profile {
-              firstName
-              lastName
-            }
-          }
-        }`,
-        variables: user
-      })
-    ).then(res => {
-      expect(res.status).toBe(200);
-      expect(res.data.user._id).toBeTruthy();
-      expect(res.data.user.username).toBe(_.toLower(user.username));
-      expect(res.data.user.email).toBe(_.toLower(user.email));
-      expect(res.data.user.profile).toBeTruthy();
-      _id = res.data.user._id;
-      return bluebird.resolve(res.data.user);
-    });
+  beforeAll(async () => {
+    const user = await createUser;
+    _id = user._id;
+    return user;
   });
 
   it("should return user with _id", done => {
